90254cb7b8b6022c042b04ae6ea4d61a5aef9591
Manoj Yadav <manojkumar.yadav@kantar.com>
2019-10-17 22:28:38 +0530

    This is my first commit

---
+++ src/com/test/AutomationFramework.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AutomationFramework {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Manoj\\Project\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in");
+		driver.quit();
+
+	}
+
+}
